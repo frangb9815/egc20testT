c4d31e3d5998e4f760b8bdcc37ec8a2722896132
frangb9815 <frangb9815@gmail.com>
2020-01-08 12:27:31 -0500

    Se ha hecho los cambios correctos instalando el requirements.txt para que la aplicación pueda desplegar

---
+++ decideDev/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/osboxes/Desktop/EGC-20T-fragaibar/egc20testT/decideDev/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
